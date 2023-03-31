56862ae0f65b6da244c013b5d5162f2aa5adba51
Trần Trọng Nghĩa <nghiatrantrong188@gmail.com>
2023-03-31 10:11:39 +0700

    Add exercise SystemTime

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SystemTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+        }
+    }
+}
